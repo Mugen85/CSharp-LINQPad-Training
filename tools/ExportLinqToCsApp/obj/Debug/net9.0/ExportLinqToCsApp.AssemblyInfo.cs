3699584dc32141237f01d5d50c0a37ac59947783
MarcoMorelloDev <doppiam1@hotmail.it>
2025-05-08 17:35:30 +0200

    Added script 016 + file. md and github view file automated converted

--- tools/ExportLinqToCsApp/obj/Debug/net9.0/ExportLinqToCsApp.AssemblyInfo.cs
+++ tools/ExportLinqToCsApp/obj/Debug/net9.0/ExportLinqToCsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExportLinqToCsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be79e8ea1f2e830ea78855cca236d47441d2e62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f78ce811b63bd79e1e2a4855380ac1e7ecead3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExportLinqToCsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExportLinqToCsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
